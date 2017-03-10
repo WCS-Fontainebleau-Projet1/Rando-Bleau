14071aa6a15c4d5a82a9a7757ece23cf26a9a489
coco-b <ccl.berger@gmail.com>
2017-03-10 11:24:39 +0100

    creation du fichier delete_circuit.php

---
+++ delete_circuit.php
@@ -0,0 +1,19 @@
+<?php
+include ('modele/connexion_bdd.php');
+
+if (is_numeric($_GET['id'])){
+	echo "id est un nombre";
+
+	$query = $bdd->prepare('DELETE FROM commentaires WHERE id=?');
+	$query->execute(array($_GET['id']));
+	$query->closeCursor();
+
+	header('localisation:info_circuits.php');
+} 
+else {
+	echo "id n'est pas un nombre";
+}
+
+
+
+ ?>
