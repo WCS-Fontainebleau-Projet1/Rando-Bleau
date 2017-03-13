dd6cc78167c68fa193fad235cdb4550483454015
florenta <florent.alemany@gmail.com>
2017-03-13 07:56:32 +0100

    changement du nom de table en 'materiel' au lieu de 'article'

--- delete_materiels.php
+++ delete_materiels.php
@@ -2,19 +2,19 @@
 
 include_once('model/connexion_bdd.php');
 
-if(is_numeric($_GET['id'])){
+if(isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])){
 	
-	$id = $_GET['id'];
+	$id = htmlspecialchars($_GET['id']);
 	
-	$delete = $bdd->prepare('DELETE FROM article WHERE id=?');
+	$delete = $bdd->prepare('DELETE FROM materiel WHERE id=?');
 	$delete->execute(array($id));
 	$delete->closeCursor();
 	
-	echo 'L\'article a bien été supprimé. Vous allez redirigé dans 5 secondes';
-	header('refresh:5;url=materiels.php');
+	echo 'L\'article a bien été supprimé. Vous allez redirigé dans un instant';
+	header('refresh:3; url=materiels.php');
 }
 
 else{
-	echo 'L\'article à supprimer n\'existe pas. Vous allez redirigé dans 5 secondes';
-	header('refresh:5;url=materiels.php');
+	echo 'L\'article à supprimer n\'existe pas. Vous allez redirigé dans un instant';
+	header('refresh:3; url=materiels.php');
 }
